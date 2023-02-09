5f78d08b53111393433f341d3e6eb8bcc3a47185
VinnyDeAlmeida <vdd28@scarletmail.rutgers.edu>
2023-02-08 20:43:59 -0500

    Added Goal Script

--- Assets/02-Mission Demolition/Scripts/Goal.cs
+++ Assets/02-Mission Demolition/Scripts/Goal.cs
@@ -1,34 +1,26 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Renderer))]
 public class Goal : MonoBehaviour
 {
-	static public bool 	goalMet = false;
+    // A static field accessible by code anywhere
+    static public bool goalMet = false;
 
-	void OnTriggerEnter(Collider other) {
-		// when the trigger is hit by something
-		// check to see if it's a Projectile 
-		if (other.gameObject.tag == "Projectile") {
-			// if so, set goalMet = true
-			Goal.goalMet = true;
+    void OnTriggerEnter(Collider other) {
+      // When the trigger is hit by something
+      // Check to see if it's a Projectile
+      Projectile proj = other.GetComponent<Projectile>();
 
-			// also set the alpha of the color of higher opacity
-			Material mat = GetComponent<Renderer>().material;
-			Color c = mat.color;
-			c.a = 1;
-			mat.color = c;
-		}
-	}
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+      if (proj != null) {
+        // If so, set goalMet to true
+        Goal.goalMet = true;
+        // Also set the alpha of the color to higher opactiy
+        Material mat = GetComponent<Renderer>().material;
+        Color c = mat.color;
+        c.a = 0.75f;
+        mat.color = c;
+      }
     }
 }
